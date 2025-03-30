850b1af5edd444ddcd3eddf92b61d1087201cd15
jarolicsdk <info@a-ostergaard.com>
2025-03-30 17:01:18 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+// THIS OVERRIDES THE BLOG LAYOUT!
+export default function Home() {
   return (
-    <html lang="en">
-      <head />
-      <body>{children}</body>
-    </html>
+    <div className="p-12 text-center">
+      <h1 className="text-4xl font-bold text-slate-800">nonicnomore is live 🔥</h1>
+      <p className="mt-4 text-lg text-slate-600">And we just kicked nicotine’s ass.</p>
+    </div>
   )
 }
